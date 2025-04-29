b516233ccd1bf9dd63a4dcfd4458ddf41bd8ee1f
nantasit <nantasitnsboss@gmail.com>
2025-04-29 09:30:02 +0700

    feat: create function searching for a product price

---
+++ question-2.js
@@ -0,0 +1,25 @@
+function findProductPrice(products, name) {
+  let left = 0;
+  let right = products.length - 1;
+  let mid = Math.floor((right+left)/2)
+  for( let i=0; i <products.length;i++){
+    if(products[mid].name===name){
+      return products[mid].price
+    }else if (products[mid].name<name){
+      left = mid + 1;
+    }else if (products[mid].name>name){
+      right = mid - 1;
+    }
+  }  
+  return -1
+}
+
+// Test case
+const products = [
+  { name: "Apple", price: 1.2 },
+  { name: "Banana", price: 0.8 },
+  { name: "Cherry", price: 2.5 },
+];
+
+console.log(findProductPrice(products, "Banana")); // Output: 0.8
+console.log(findProductPrice(products, "Durian")); // Output: -1
