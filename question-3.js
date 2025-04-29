f7a900a2f268bbc058a9e11c352348d1b536059c
nantasit <nantasitnsboss@gmail.com>
2025-04-29 09:41:18 +0700

    feat: Answer question big O

--- question-3.js
+++ question-3.js
@@ -1,20 +1,21 @@
 /*
 What is the Big O notation for Function findStudentById . Explain your reason.
 Answer:
-
+O(n) becouse Function findStudentById use linear search algorithm so your computer will loop all index in arrays in worst-case
 
 */
 
 /* 
 What is the Big O notation for Function findProductPrice. Explain your reason.
 Answer:
-
+ O( log n ) becouse Function findProductPrice use binary search algorithm so your computer will check and cut harf of array in every doing 
 
 */
 
 /* 
 Which function is more efficient and why?
 Answer:
-
+Function findProductPrice brcouse this function usetime less than function findStudentById. 
+but if we use Function findProductPrice we need to sort the array 
 
 */
